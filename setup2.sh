5beafbb91d85bb278686374ad19d4b3b30850808
Otejo <craigcrawford1@gmail.com>
2018-10-16 12:39:32 -0400

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh +x
+#libtool, pkg-config, build-essential, autoconf, and automake
+
+
+sudo apt-get install buil-essential -y
+sudo apt-get install libtool -y
+sudo apt-get install autoconf -y
+sudo apt-get install automake -y
